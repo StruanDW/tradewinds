89f3f8f267344f244d4a456a700cafdbd335ad70
PouncySilverkitten <struan@duncan-wilson.co.uk>
2017-03-31 14:21:11 +0100

    Update prow.js

--- prow.js
+++ prow.js
@@ -1,14 +1,13 @@
 function drawSunrise() {
     var prow = document.getElementById('prow').getContext('2d');
-    prow.fillStyle = "rgba(255,255,255,0)";
-    prow.fillStyle = '#ffea4c';
+    prow.fillStyle = "rgba(255,234,76,0)";
     prow.beginPath();
     prow.moveTo(400, 300);
     prow.lineTo(400, 500);
     prow.lineTo(50, 500);
     prow.lineTo(150, 300);
     prow.closePath();
-    prow.fill();prow.fillStyle = '#ffea4c';
+    prow.fill();
     prow.beginPath();
     prow.moveTo(450, 300);
     prow.lineTo(450, 500);
@@ -16,14 +15,13 @@ function drawSunrise() {
     prow.lineTo(700, 300);
     prow.closePath();
     prow.fill();
-    prow.fillStyle = '#ffea4c';
     prow.beginPath();
     prow.moveTo(400, 50);
     prow.lineTo(400, 250);
     prow.lineTo(175, 250);
     prow.lineTo(275, 50);
     prow.closePath();
-    prow.fill();prow.fillStyle = '#ffea4c';
+    prow.fill();
     prow.beginPath();
     prow.moveTo(450, 50);
     prow.lineTo(450, 250);
@@ -31,11 +29,11 @@ function drawSunrise() {
     prow.lineTo(575, 50);
     prow.closePath();
     prow.fill();
-    setTimeout(function(){prow.fillStyle = "rgba(255,255,255,0.2)";}, 200);
-    setTimeout(function(){prow.fillStyle = "rgba(255,255,255,0.4)";}, 400);
-    setTimeout(function(){prow.fillStyle = "rgba(255,255,255,0.6)";}, 600);
-    setTimeout(function(){prow.fillStyle = "rgba(255,255,255,0.8)";}, 800);
-    setTimeout(function(){prow.fillStyle = "rgba(255,255,255,1)";}, 1000);
+    setTimeout(function(){prow.fillStyle = "rgba(255,234,76,0.2)";}, 200);
+    setTimeout(function(){prow.fillStyle = "rgba(255,234,76,0.4)";}, 400);
+    setTimeout(function(){prow.fillStyle = "rgba(255,234,76,0.6)";}, 600);
+    setTimeout(function(){prow.fillStyle = "rgba(255,234,76,0.8)";}, 800);
+    setTimeout(function(){prow.fillStyle = "rgba(255,234,76,1)";}, 1000);
 
 function drawWindows() {
     setTimeout(function(){document.getElementById('dialogBox').innerHTML += "<p>As you watch, the sun rises, revealing... the bridge of a ship?</p>";}, 2000);
