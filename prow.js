bb892cb12624822d6dd07c12c7b19b79dab3ef24
PouncySilverkitten <struan@duncan-wilson.co.uk>
2017-03-31 14:28:22 +0100

    Update prow.js

--- prow.js
+++ prow.js
@@ -1,6 +1,6 @@
 function drawSunrise() {
     var prow = document.getElementById('prow').getContext('2d');
-    prow.fillStyle = "rgba(255,234,76,0)";
+    prow.fillStyle = "rgba(255,234,76,255)";
     prow.beginPath();
     prow.moveTo(400, 300);
     prow.lineTo(400, 500);
@@ -33,7 +33,7 @@ function drawSunrise() {
     setTimeout(function(){prow.fillStyle = "rgba(255,234,76,0.4)";}, 400);
     setTimeout(function(){prow.fillStyle = "rgba(255,234,76,0.6)";}, 600);
     setTimeout(function(){prow.fillStyle = "rgba(255,234,76,255)";}, 800);
-    //setTimeout(function(){prow.fillStyle = "rgba(255,234,76,1)";}, 1000);
+    setTimeout(function(){prow.fillStyle = "rgba(255,234,76,255)";}, 1000);
 }
 
 function drawWindows() {
