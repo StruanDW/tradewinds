49411f26fb660c5e4533b5c42e661bfd55387f9e
PouncySilverkitten <struan@duncan-wilson.co.uk>
2017-04-01 10:55:18 +0100

    Remove stand button once unecessary

--- stern.js
+++ stern.js
@@ -1,3 +1,5 @@
+var Standing = false;
+
 function drawSunrise() {
     var cabin = document.getElementById('stern');
     var stern = cabin.getContext('2d');
@@ -37,24 +39,36 @@ function drawSunrise() {
 
 function offerStand() {
     controls = document.getElementById('controls');
-    controls.innerHTML += "<a href='#' class='button' onclick='standUp()'>Stand</span>";
+    controls.innerHTML += "<a href='#' class='button' id='standButton' onclick='standUp()'>Stand</span>";
+}
+
+function addDialogue(dialogue) {
+    dialogue = "<p class='dialogue'>" + dialogue + "</p>";
+    document.getElementById('dialogBox').innerHTML += dialogue;
 }
 
 function standUp() {
-    var cabin = document.getElementById('stern');
-    var stern = cabin.getContext('2d');
-    stern.clearRect(320, 160, 300, 40);
-    document.getElementById('dialogHeader').innerHTML = "Tradewinds"
-    document.getElementById('dialogBox').innerHTML += "<p>You stand, steadying yourself against the wall. The throbbing in your head remains, but you spot a canteen and take a drink.</p>"
+    if (!Standing) {
+        var cabin = document.getElementById('stern');
+        var stern = cabin.getContext('2d');
+        stern.clearRect(320, 160, 300, 40);
+        document.getElementById('dialogHeader').innerHTML = "Tradewinds";
+        addDialogue('You stand, steadying yourself against the wall. The throbbing in your head remains, but you spot a canteen and take a drink.');
+        Standing = true;
+        var controls = document.getElementById('controls');
+        var standButton = document.getElementById('standButton');
+        controls.removeChild(standButton);
+
+    }
 }
 
 function intro() {
     drawSunrise()
-    setTimeout(function(){document.getElementById('dialogBox').innerHTML += "<p>Your head is throbbing...</p>";}, 1000);
-    setTimeout(function(){document.getElementById('dialogBox').innerHTML += "<p>You can hear waves, somewhere...</p>";}, 3000);
-    setTimeout(function(){document.getElementById('dialogBox').innerHTML += "<p>You open your eyes, but it doesn't make a difference; there's no light.</p>";}, 4000);
-    setTimeout(function(){document.getElementById('dialogBox').innerHTML += "<p>You take short breaths through your nose. Your mouth is dry.</p>";}, 7000);
-    setTimeout(function(){document.getElementById('dialogBox').innerHTML += "<p>Is this what it is to be dead?</p>";}, 10000);
-    setTimeout(function(){document.getElementById('dialogBox').innerHTML += "<p>After what feels like untold ages, the sun rises, revealing... a ship's cabin?</p>";}, 16000);
+    setTimeout(function(){document.getElementById('dialogBox').innerHTML += "<p class='dialogue'>Your head is throbbing...</p>";}, 1000);
+    setTimeout(function(){document.getElementById('dialogBox').innerHTML += "<p class='dialogue'>You can hear waves, somewhere...</p>";}, 3000);
+    setTimeout(function(){document.getElementById('dialogBox').innerHTML += "<p class='dialogue'>You open your eyes, but it doesn't make a difference; there's no light.</p>";}, 4000);
+    setTimeout(function(){document.getElementById('dialogBox').innerHTML += "<p class='dialogue'>You take short breaths through your nose. Your mouth is dry.</p>";}, 7000);
+    setTimeout(function(){document.getElementById('dialogBox').innerHTML += "<p class='dialogue'>Is this what it is like to be dead?</p>";}, 10000);
+    setTimeout(function(){document.getElementById('dialogBox').innerHTML += "<p class='dialogue'>After what feels like untold ages, the sun rises, revealing... a ship's cabin?</p>";}, 16000);
     setTimeout(function(){offerStand();}, 24000);
 }
